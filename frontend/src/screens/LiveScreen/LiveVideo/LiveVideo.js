19ee1e297f9eb87ae7459db895342b66bbbb7637
amanc1248 <amanc1248@gmail.com>
2022-07-04 16:54:19 +0545

    Add: background image to livevideo

--- frontend/src/screens/LiveScreen/LiveVideo/LiveVideo.js
+++ frontend/src/screens/LiveScreen/LiveVideo/LiveVideo.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function LiveVideo() {
-  return <div className="liveVideo__div">LiveVideo</div>;
+  return (
+    <div className="liveVideo__div">
+      <div className="background__video">
+        <img
+          src="https://res.cloudinary.com/proudposhak-com/image/upload/v1656932744/aestheticproject/livevideo_display_image_yhdcbt.jpg"
+          alt="livevideo"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default LiveVideo;
