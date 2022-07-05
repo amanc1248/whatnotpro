d883a6535bf512ff6b17c65d47421c6dc969ee1c
amanc1248 <amanc1248@gmail.com>
2022-07-05 16:24:06 +0545

    Add: set chat section

--- frontend/src/screens/LiveScreen/Chat/Chat.js
+++ frontend/src/screens/LiveScreen/Chat/Chat.js
@@ -1,7 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "../../../styles/Chat.css";
+import AskQuestion from "./AskQuestion";
+import JustChat from "./JustChat";
 function Chat() {
-  return <div className="chat__div">Chat</div>;
+  const [chatType, setChatType] = useState(0);
+  const changeToChat = () => {
+    setChatType(0);
+  };
+  const changeToAskAnything = () => {
+    setChatType(1);
+  };
+  return (
+    <div className="chat__div">
+      <div className="chat__askanything__content">
+        {chatType === 0 ? <JustChat></JustChat> : <AskQuestion></AskQuestion>}
+      </div>
+      <div className="chat__two__buttons">
+        <div className="chat__button" onClick={changeToChat}>
+          Chat
+        </div>
+        <div className="askAnything__button" onClick={changeToAskAnything}>
+          Ask Anything
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
---
+++ frontend/src/screens/LiveScreen/Chat/JustChat.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function JustChat() {
+  return <div>JustChat</div>;
+}
+
+export default JustChat;
